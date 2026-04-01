cf5f50980fb043b841000f8ae80f8d4bc63f0148
Julia Szcześniak <257291@edu.p.lodz.pl>
2026-04-01 14:38:52 +0200

    Dodanie alogrytmu dfs z ograniczeniem 20, heurystyki

---
+++ SISE/dfs.py
@@ -0,0 +1,30 @@
+from ukladanka import *
+def dfs(initial_board, rows, cols, order="LUDR"):
+    s = tuple(tuple(row) for row in initial_board)
+    goal = get_target_board(rows, cols)
+    if s == goal:
+        return "", 1, 0, 0
+    stack = [(s, 0)]
+    T = set()  # Stany zamknięte
+    in_stack = {s: (None, None)}  # Odpowiednik ~S.has(n) i historia
+
+    processed_count = 0
+    max_depth = 0
+    visited_count = 1
+
+    while stack:
+        v, depth = stack.pop()  # Ściągamy z GÓRY (LIFO)
+        processed_count += 1
+        max_depth = max(max_depth, depth)
+        T.add(v)  # Dodajemy do zamkniętych
+        if depth < 20:
+            # Odwracamy sąsiadów, żeby pierwszy ruch z order był na szczycie stosu
+            for n, move in reversed(get_neighbors(v, rows, cols, order)):
+                if n not in T and n not in in_stack:
+                    visited_count += 1
+                    in_stack[n] = (v, move)
+                    if n == goal:
+                        path = reconstruct_path(in_stack, n)
+                        return path, visited_count, processed_count, depth + 1
+                    stack.append((n, depth + 1))
+    return "-1", visited_count, processed_count, max_depth
---
+++ SISE/heurystyka.py
@@ -0,0 +1,35 @@
+from ukladanka import get_target_board
+
+def get_target_positions(rows, cols):
+    """Tworzy słownik {wartość: (wiersz, kolumna)} dla stanu docelowego."""
+    target_board = get_target_board(rows, cols)
+    positions = {}
+    for r in range(rows):
+        for c in range(cols):
+            val = target_board[r][c]
+            positions[val] = (r, c)
+    return positions
+
+def heuristic_manhattan(board, rows, cols, target_positions):
+    """Liczy jak daleko pola są od swoich prawidłowych miejsc, suma kroków, aby dotrzeć do celu"""
+    score = 0
+    for r in range(rows):
+        for c in range(cols):
+            val = board[r][c]
+            if val != 0:  # Zera nie liczymy
+                # Pobieramy współrzędne celu ze słownika
+                target_r, target_c = target_positions[val]
+                # Liczymy różnicę wierszy i kolumn
+                score += abs(r - target_r) + abs(c - target_c)
+    return score
+
+
+def heuristic_hamming(board, target_board, rows, columns):
+    """Liczy ile pól jest na swoim miejscu"""
+    score = 0
+    for r in range(rows):
+        for c in range(columns):
+            val = board[r][c]
+            if val != 0 and val != target_board[r][c]:
+                score += 1
+    return score
